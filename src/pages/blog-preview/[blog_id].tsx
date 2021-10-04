c3e0050102bed7cc4b39631a2f01aa62a742d99f
Sivadinesh M <sivadinesh@gmail.com>
2021-10-04 23:35:09 +0530

    blog-preview-added

---
+++ src/pages/blog-preview/[blog_id].tsx
@@ -0,0 +1,16 @@
+const Blog = ({ todos }) => {
+	// render code
+	return (
+		<div>
+			<h1>Todos</h1>
+
+			<div className='laptop'>
+				<div className='content'>
+					<iframe src='https://www.ndtv.com' style={{ width: '100%', border: 'none', height: '100%' }} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
