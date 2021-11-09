89d6ed36ff018c3c2e6e14fed755cb5fc98ddb74
aalamhub <82435830+aalamhub@users.noreply.github.com>
2021-11-09 19:52:04 +0530

    reference url

--- public/Script/script006.sql
+++ public/Script/script006.sql
@@ -1,3 +1,9 @@
 alter table company add column sub_domain varchar(100);
 alter table lead_page add column slug varchar(350);
 UPDATE lead_page lp SET slug=lower(replace (trim(lp.lead_page_name),' ','-' ));
+
+--http://localhost:3000/blog/test2
+--http://localhost:3000/view/b/test/test2
+
+--http://localhost:3000/leads/test-thumbnail
+--http://localhost:3000/view/lp/test/test-thumbnail
